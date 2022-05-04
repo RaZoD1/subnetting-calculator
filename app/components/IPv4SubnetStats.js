94d570e857dbdddf8da0c7d7792563615d1d8d89
Raphael Freunberger <raphaelfreunberger@gmail.com>
2022-05-04 12:25:03 +0200

    stuff

---
+++ app/components/IPv4SubnetStats.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { useState } from 'react';
+
+export default function IPv4SubnetStats(props) {
+
+  return (
+    <div>
+      <h1>IPv4SubnetStats</h1>
+    </div>
+  );
+}
