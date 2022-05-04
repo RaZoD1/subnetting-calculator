bfc0633907ef208338f62266bcec1efff18e0af8
Raphael Freunberger <raphaelfreunberger@gmail.com>
2022-05-04 18:14:19 +0200

    New Features for IPv4 Addresses and Subnetmasks

--- app/utils/IPv4.js
+++ app/utils/IPv4.js
@@ -6,36 +6,107 @@ export class IPv4 {
     if(!IPv4.isValid(ipString)) {
       throw new Error("Invalid IPv4 address");
     }
-    
   }
 
-  get octets() {
+  getStringOctets() {
     return this.ipString.split(".");
   }
-
-
-  get binary() {
-    return this.octets.map(octet => {
-      return parseInt(octet).toString(2).padStart(8, '0');
-    });
-  }
-  getBinaryString(seperator) {
-    return this.binary.join(seperator);
-  }
-
-  get decimal() {
-    return this.octets.map(octet => {
+  
+  getDecimalOctets() {
+    return this.stringOctets.map(octet => {
       return parseInt(octet);
     }
     );
   }
+
+  getBinaryString(seperator) {
+    seperator = seperator || '.';
+    return this.getDecimalOctets().map(octet => {
+      return octet.toString(2).padStart(8, '0');
+    }).join(seperator);
+  }
+
   getDecimalString(seperator) {
+    seperator = seperator || '.';
     return this.decimal.join(seperator);
   }
 
+  getBytes(){
+    return this.decimal.reduce((prev, curr) => {
+      return prev * 256 + curr;
+    }, 0);
+  }
+
+  static fromBytes(bytes) {
+    let ipString = '';
+    let i;
+    for(i = 0; i < 4; i++){
+      ipString += bytes >>> (i * 8) & 0xff;
+      if(i !== 3) {
+        ipString += '.';
+      }
+    }
+    return new IPv4(ipString);
+  }
 
   static isValid(ipString) {
     return ipString.match(regexIPv4);
   }
+  
+}
 
+export class IPv4Subnetmask{
+  constructor(maskString) {
+    this.maskString = maskString;
+
+    if(!this.isValid()) {
+      throw new Error("Invalid IPv4 Subnetmask");
+    }
+
+    this.format = IPv4.isValid(maskString) ? 'dottedDecimal' : 'prefixLength';
+    if(this.format = 'dottedDecimal') {
+      this.ipv4 = new IPv4(maskString);
+      this.bytes = this.ipv4.getBytes();
+    } else if(this.format = 'prefixLength') {
+      this.prefixLength = parseInt(maskString);
+      this.bytes = IPv4Subnetmask.getBytesFromPrefixLength(this.prefixLength);
+      this.ipv4 = IPv4.fromBytes(this.bytes);
+    }
+  }
+
+  
+  
+  isValid(){
+    return IPv4Subnetmask.isValid(this.maskString);
+  }
+  
+  static getBytesFromPrefixLength(prefixLength) {
+    return Math.pow(2, prefixLength-1) << (32 - prefixLength);
+  }
+
+  static isValid(maskString) {
+    if(IPv4.isValid(maskString)){
+      this.ipv4 = new IPv4(maskString);
+      let mask = this.ipv4.getBytes();
+      
+      let i;
+      // check if beginning bits are 1s
+      for(i = 31; i >= 0; i--){
+        if(mask & (1 << i) == 0)
+          break;
+      }
+      // check if ending bits are 0s
+      for(; i>= 0; i--){
+        if(mask & (1 << i) != 0)
+          return false;
+      }
+      return true;
+      
+    }
+    if(!isNaN(maskString)){
+      mask = parseInt(maskString);
+      return mask >= 0 && mask <= 32;
+    }
+    return false;
+  }
 }
